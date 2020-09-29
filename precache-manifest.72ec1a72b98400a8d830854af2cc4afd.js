79667cf97d51249dc38dd44624ea7f61d31bd380
Pavan <pavang707@gmail.com>
2020-09-29 14:25:56 +0530

    Updates

---
+++ precache-manifest.72ec1a72b98400a8d830854af2cc4afd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "478c2dcde8ee8e925ac5d2a62fa12bda",
+    "url": "/login/index.html"
+  },
+  {
+    "revision": "ccfb574736e0fb555ce4",
+    "url": "/login/static/css/main.68c0c4ef.chunk.css"
+  },
+  {
+    "revision": "80c4fd06a2fdc8a879c0",
+    "url": "/login/static/js/2.e34b9169.chunk.js"
+  },
+  {
+    "revision": "a1212b395e88f070a42368ea2ff93e06",
+    "url": "/login/static/js/2.e34b9169.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccfb574736e0fb555ce4",
+    "url": "/login/static/js/main.7869534a.chunk.js"
+  },
+  {
+    "revision": "2a6ddebc308cbef80bfa",
+    "url": "/login/static/js/runtime-main.eced9b5c.js"
+  },
+  {
+    "revision": "1c7cac7db15a7ea67bcf12a0141b0077",
+    "url": "/login/static/media/n1.1c7cac7d.jpg"
+  }
+]);
